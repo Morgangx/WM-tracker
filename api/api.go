9a2539f48e5eabc18f788b756081ccf6ec3af996
Morgangx <morgangbulawa@gmail.com>
2026-02-19 18:16:38 +0100

    Initial commit of Go files

---
+++ api/api.go
@@ -0,0 +1,46 @@
+package wmtracker
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const baseUrl string = "https://api.warframe.market/v2/"
+
+func GetTopOrders(item_slug string, rank string) (*TopOrdersResponse, error) {
+	rankSuff := ""
+	if rank != "0" {
+		rankSuff = fmt.Sprintf("?rank=%s", rank)
+	}
+	fullUrl := baseUrl + "orders/item/" + item_slug + "/top" + rankSuff
+	res, err := http.Get(fullUrl)
+	if err != nil {
+		return nil, fmt.Errorf("Could not get response: %w", err)
+	}
+	defer res.Body.Close()
+
+	var data TopOrdersResponse
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&data)
+	if err != nil {
+		return nil, fmt.Errorf("Error decoding response: %w", err)
+	}
+	return &data, nil
+}
+
+func GetOrders(item_slug string) (*OrdersResponse, error) {
+	fullUrl := baseUrl + "orders/item/" + item_slug
+	res, err := http.Get(fullUrl)
+	if err != nil {
+		return nil, fmt.Errorf("Could not get response: %w", err)
+	}
+	defer res.Body.Close()
+	var data OrdersResponse
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&data)
+	if err != nil {
+		return nil, fmt.Errorf("Error decoding response: %w", err)
+	}
+	return &data, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module WM_tracker
+
+go 1.25.7
